f15429b66d73af5ee668f24e4cac54a76f40433f
elcresan <elcresan@gmail.com>
2012-11-22 01:05:41 +0100

    Eliu
    Probando a commitear. Agrego un string

--- gen/com/adm/book/R.java
+++ gen/com/adm/book/R.java
@@ -23,6 +23,7 @@ public final class R {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
+        public static final int PruebaEliu=0x7f040003;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
