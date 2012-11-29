23518c3d54c1bafff8e8ea2b22672cd80982de6b
Mario Sanz <seiseises@gmail.com>
2012-11-29 15:24:09 +0100

    Añadido TabLayout en MainActivity

--- gen/com/example/bookache/R.java
+++ gen/com/example/bookache/R.java
@@ -14,11 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int botonprueba=0x7f070003;
-        public static final int button1=0x7f070001;
-        public static final int menu_settings=0x7f070004;
-        public static final int textView1=0x7f070000;
-        public static final int textoprueba=0x7f070002;
+        public static final int botonprueba=0x7f070005;
+        public static final int button1=0x7f070000;
+        public static final int menu_settings=0x7f070006;
+        public static final int tab1=0x7f070001;
+        public static final int tab2=0x7f070002;
+        public static final int tab3=0x7f070003;
+        public static final int textoprueba=0x7f070004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
