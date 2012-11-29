1086840bd14a47914437f49308c1b32f67ce7092
Mario Sanz <seiseises@gmail.com>
2012-11-29 16:56:25 +0100

    Añadida ListView con varios elementos de prueba en MainActivity
    Tranqui Nico, tu botón sigue estando ahí jaja

--- src/com/example/bookache/MainActivity.java
+++ src/com/example/bookache/MainActivity.java
@@ -1,18 +1,46 @@
 package com.example.bookache;
 
+import java.util.ArrayList;
+
 import com.example.bookache.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	
+	ListView lvDatos;
+
+	String strings[] = new String[]{
+			"All", 
+			"Wishlist",
+			"Givelist",
+			"Custom list 1",
+			"Custom list 2",
+			"Custom list 3",
+			"Custom list 4",
+			"Custom list 5",
+			"Custom list 6",
+			"Custom list 7"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvDatos = (ListView) findViewById(R.id.listView1);
+    	
+    	//final ArrayList<String> mLista = new ArrayList<String>();
+    	final ArrayAdapter<String> mAdapter = 
+    			new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, strings/*mLista*/);
+    	lvDatos.setAdapter(mAdapter);
+        
+        //mLista.add("Lista Prueba 1");
+        mAdapter.notifyDataSetChanged();
     }
 
     @Override
