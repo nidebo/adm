990cc2a03fdc6210a43588fc32972634c4bc0278
Mario Sanz <seiseises@gmail.com>
2012-12-10 12:40:20 +0100

    commit bueno 4

---
+++ src/com/example/boox/BookListActivity.java
@@ -0,0 +1,31 @@
+package com.example.boox;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+
+public class BookListActivity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.book_list_activity);
+        
+        if (savedInstanceState == null) {
+            // During initial setup, plug in the details fragment.
+            BookList booklist = new BookList();
+            booklist.setArguments(getIntent().getExtras());
+            getSupportFragmentManager()
+            	.beginTransaction()
+            	.add(android.R.id.content, booklist)
+            	.commit();
+        }
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.book_list, menu);
+        return true;
+    }*/
+}
