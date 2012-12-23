bd9e53e0e50dc18823079b9eb37a7623c3b1719e
nicolas <nicodeboni@gmail.com>
2012-12-23 22:21:53 +0100

    GuardarLista -> Done

--- src/listasLibros/GestorListas.java
+++ src/listasLibros/GestorListas.java
@@ -90,35 +90,35 @@ public class GestorListas {
 	
 	/*
 	 * Dos opciones, por una parte, la de las estructuras que he dejado aqui:
-	 * 	básicamente es tratar como diferentes guardar y borrar lista
-	 * 	asi se podría borrar cuando te de la gana y guardar... pues igual
-	 *  el problema de esto es que es muy manual y habría que tener siempre
+	 * 	bï¿½sicamente es tratar como diferentes guardar y borrar lista
+	 * 	asi se podrï¿½a borrar cuando te de la gana y guardar... pues igual
+	 *  el problema de esto es que es muy manual y habrï¿½a que tener siempre
 	 *  conexion a la red.
 	 *  
 	 *  Por otra parte, cosa que yo creo que es mejor idea pero no se si es posible:
-	 *  Unir las dos funciones guardar y borrar listas para que únicamente
-	 *  se llame cuando se vaya a cerrar la aplicación. Habria que detectar que hayan
-	 *  o no habido cambios (esto es sencillo, con un flag), y sería una funcion
-	 *  actualizar que ya llamase a guardar o borrar libros de ListaLibros, que sería
+	 *  Unir las dos funciones guardar y borrar listas para que ï¿½nicamente
+	 *  se llame cuando se vaya a cerrar la aplicaciï¿½n. Habria que detectar que hayan
+	 *  o no habido cambios (esto es sencillo, con un flag), y serï¿½a una funcion
+	 *  actualizar que ya llamase a guardar o borrar libros de ListaLibros, que serï¿½a
 	 *  desde donde se accede al servidor.
-	 *  La funcion esta tendría que ser llamada al cerrar la aplicación (de aqui el no
+	 *  La funcion esta tendrï¿½a que ser llamada al cerrar la aplicaciï¿½n (de aqui el no
 	 *  sabes si esto es posible de hacer) cuando se hayan detectado cambios y haya
-	 *  conexión a internet (cada x tiempo).
+	 *  conexiï¿½n a internet (cada x tiempo).
 	 * */
-	public void guardaLista(String nombreLista){//Necesitará nombre de usuario
+	public void guardaLista(String nombreLista, String uname){//Necesitarï¿½ nombre de usuario
 		ListaLibros lis = null;
 		for (int i=0; i<lista.size(); i++) { 
 			lis = lista.get(i);
 			if (lis.getNombreLista()==nombreLista)
 				i=lista.size();
 		}
-		lis.guardaListaLibros();//Necesitará nombre de usuario
+		lis.guardaListaLibros(nombreLista,uname);//Necesitarï¿½ nombre de usuario
 	}
 	
 	
 	public void cargaListaLibros(){
-		//No haría falta identificador, se supone que esto se haría al iniciar
-		//la aplicación
+		//No harï¿½a falta identificador, se supone que esto se harï¿½a al iniciar
+		//la aplicaciï¿½n
 
 	}
 	
--- src/listasLibros/ListaLibros.java
+++ src/listasLibros/ListaLibros.java
@@ -1,21 +1,12 @@
 package listasLibros;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -24,14 +15,7 @@ import org.apache.http.protocol.HTTP;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.content.Intent;
 import android.os.AsyncTask;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.example.boox.R;
-import com.example.boox.TabsActivity;
-import com.example.boox.Main.AsyncLogin;
 
 public class ListaLibros {
 	
@@ -91,12 +75,13 @@ public class ListaLibros {
 	}
 	
 	//Funciones relativas al servidor
-	public void guardaListaLibros(){
+	public void guardaListaLibros(String lname, String uname){
 		//Nico haz algo aqui para meter la lista en el servidor
 		//La funcion deber� recibir el nombre o id del usuario, supongo y usar s_NombreLista
 		
 		//Habr�a que comprobar si existe la lista y modificarla, y sino, crearla
-		
+		AsyncSave dl = new AsyncSave();
+		dl.execute(lname, uname);
 	}
 	
 	
@@ -125,6 +110,55 @@ public class ListaLibros {
 
 			HttpPut request = new HttpPut("http://boox.eu01.aws.af.cm/" +
 					params[1] + "/delCustomList"); 
+			
+			JSONObject json = new JSONObject();
+			try {
+				json.put("uname", params[0]);
+				json.put("custom", params[1]);
+	            StringEntity se;
+				se = new StringEntity(json.toString());
+	            se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+	            request.setEntity(se);
+				HttpResponse response = client.execute(request);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClientProtocolException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} 
+			     
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+
+
+		}
+	}
+	
+	
+public class AsyncSave extends AsyncTask<String, Void, Void> {
+		
+		@Override
+		protected void onPreExecute(){
+
+		}
+		
+		@Override
+		protected Void doInBackground(String... params) {
+			//////////////
+			HttpClient client = new DefaultHttpClient();
+
+			HttpPut request = new HttpPut("http://boox.eu01.aws.af.cm/" +
+					params[1] + "/addCustomList"); 
 			
 			JSONObject json = new JSONObject();
 			try {
