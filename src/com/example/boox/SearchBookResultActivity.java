1e20b002613802eee32eb1243b9d7a55d5e16fee
Eliu <elcresan@gmail.com>
2013-01-06 03:51:42 +0100

    Cambiado activity result search
    La actividad result search ahora extiende ListActivity en lugar de
    activity, ahora al pulsar llama a la actividad de mostrar los datos de
    un libro, esa actividad recibe el ISBN mediante un extra del intent.
    Falta o que lo busque de internet o de la BD interna.

--- src/com/example/boox/SearchBookResultActivity.java
+++ src/com/example/boox/SearchBookResultActivity.java
@@ -7,19 +7,24 @@ import com.example.boox.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 import apiGoogle.InterfazAPI;
 
-public class SearchBookResultActivity extends Activity {
-
+public class SearchBookResultActivity extends ListActivity {
+	ArrayList<Libro> libros = new ArrayList<Libro>();
+	Context context = this;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_search_book_result);
-		ListView lv = (ListView) findViewById(R.id.ListView1);
 		
 		Bundle extras = getIntent().getExtras();
 		int modo = extras.getInt("modo");
@@ -27,16 +32,14 @@ public class SearchBookResultActivity extends Activity {
 		
 		
 		InterfazAPI api = new InterfazAPI();
-		String[] titulos = {" ", " ", " " ," ", " ", " " ," ", " ", " " ," "};
-		ArrayList<Libro> libros = null;
+		ArrayList<String> titulos = new ArrayList<String>();
 
-		
+
 		if(modo == 0){
 			libros = api.ObtenerListaLibrosPorTitulo(cont);
 			if(libros != null){
 				for(int i=0; i < libros.size(); i++)
-					titulos[i] = libros.get(i).getTitulo();
-				lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titulos));
+					titulos.add(libros.get(i).getTitulo());
 			}
 			else{
 				Toast to = new Toast(this);
@@ -49,8 +52,7 @@ public class SearchBookResultActivity extends Activity {
 				libros = api.ObtenerListaLibrosPorAutor(cont);
 				if(libros != null){
 					for(int i=0; i < libros.size(); i++)
-						titulos[i] = libros.get(i).getTitulo();
-					lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titulos));
+						titulos.add(libros.get(i).getTitulo());
 				}
 				else{
 					Toast to = new Toast(this);
@@ -63,8 +65,7 @@ public class SearchBookResultActivity extends Activity {
 					Libro libro = new Libro();
 					libro = api.ObtenerLibroPorIsbn(cont);
 					if(libro != null){
-						titulos[0] = libro.getTitulo();
-						lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titulos));
+						titulos.add(libro.getTitulo());
 					}
 					else{
 						Toast to = new Toast(this);
@@ -74,13 +75,27 @@ public class SearchBookResultActivity extends Activity {
 				}
 			}
 		}
+		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titulos));				
 	}
 
+	@Override
+	protected void onListItemClick(ListView list, View view, int position, long id){
+		super.onListItemClick(list, view, position, id);
+        Intent i = new Intent();
+        i.setClass(context, BookActivity.class);
+        i.putExtra("isbn", libros.get(position).getIsbn());
+        startActivity(i);
+	}
+		
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		getMenuInflater().inflate(R.menu.activity_search_book_result, menu);
 		return true;
 	}
+	
 
+	
+	
 }
