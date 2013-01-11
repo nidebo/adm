2e7c30b6755fae250b88dfc94368261bfe0fd927
Mario Sanz <seiseises@gmail.com>
2013-01-11 04:06:08 +0100

    Añadido botón "Share with" en la ActionBar de Book.

--- gen/com/example/boox/R.java
+++ gen/com/example/boox/R.java
@@ -47,15 +47,16 @@ public final class R {
         public static final int login_password=0x7f090037;
         public static final int login_username=0x7f090036;
         public static final int menu_settings=0x7f090039;
-        public static final int menu_settings_book_list=0x7f09003f;
-        public static final int menu_settings_detalles_libro=0x7f090045;
+        public static final int menu_settings_book_list=0x7f09003a;
+        public static final int menu_settings_detalles_libro=0x7f090046;
         public static final int pager=0x7f09002d;
-        public static final int search=0x7f09003e;
-        public static final int search_book_list=0x7f090044;
+        public static final int search=0x7f090045;
+        public static final int search_book_list=0x7f090040;
         public static final int searchby_spinner=0x7f090022;
         public static final int set_confirm_password=0x7f090026;
         public static final int set_new_password=0x7f090025;
         public static final int set_new_username=0x7f090024;
+        public static final int share=0x7f09003f;
         public static final int signup_button=0x7f09002c;
         public static final int signup_confirm_password=0x7f090029;
         public static final int signup_fullname=0x7f09002a;
@@ -63,15 +64,15 @@ public final class R {
         public static final int signup_username=0x7f090027;
         public static final int signup_zipcode=0x7f09002b;
         public static final int state=0x7f09001a;
-        public static final int subitem1=0x7f09003a;
-        public static final int subitem1_book_list=0x7f090040;
-        public static final int subitem1_detalles_libro=0x7f090046;
-        public static final int subitem2=0x7f09003b;
-        public static final int subitem2_book_list=0x7f090041;
-        public static final int subitem3=0x7f09003c;
-        public static final int subitem3_book_list=0x7f090042;
-        public static final int subitem4=0x7f09003d;
-        public static final int subitem4_book_list=0x7f090043;
+        public static final int subitem1=0x7f090041;
+        public static final int subitem1_book_list=0x7f09003b;
+        public static final int subitem1_detalles_libro=0x7f090047;
+        public static final int subitem2=0x7f090042;
+        public static final int subitem2_book_list=0x7f09003c;
+        public static final int subitem3=0x7f090043;
+        public static final int subitem3_book_list=0x7f09003d;
+        public static final int subitem4=0x7f090044;
+        public static final int subitem4_book_list=0x7f09003e;
         public static final int tableRow0=0x7f090001;
         public static final int tableRow1=0x7f090003;
         public static final int tableRow2=0x7f090005;
@@ -134,18 +135,18 @@ public final class R {
         public static final int detalles_libro=0x7f08000e;
     }
     public static final class string {
-        public static final int about_author1=0x7f040047;
-        public static final int about_author2=0x7f040048;
-        public static final int about_author3=0x7f040049;
-        public static final int about_author4=0x7f04004a;
-        public static final int about_author5=0x7f04004b;
-        public static final int about_authors_title=0x7f040046;
+        public static final int about_author1=0x7f040048;
+        public static final int about_author2=0x7f040049;
+        public static final int about_author3=0x7f04004a;
+        public static final int about_author4=0x7f04004b;
+        public static final int about_author5=0x7f04004c;
+        public static final int about_authors_title=0x7f040047;
         /**  Main 
          */
         public static final int app_name=0x7f040003;
         public static final int bar_search=0x7f04000e;
         public static final int bar_search_book_list=0x7f040015;
-        public static final int bar_search_detalles_libro=0x7f04001d;
+        public static final int bar_search_detalles_libro=0x7f04001e;
         public static final int example_author=0x7f040002;
         public static final int example_subtitle=0x7f040001;
         /**  Libro de ejemplo 
@@ -153,42 +154,43 @@ public final class R {
         public static final int example_title=0x7f040000;
         /**  Friends 
          */
-        public static final int friends_internet_error=0x7f040026;
+        public static final int friends_internet_error=0x7f040027;
         public static final int hello_world=0x7f040004;
-        public static final int login_button_text=0x7f04002d;
-        public static final int login_internet_error=0x7f040032;
-        public static final int login_invalid=0x7f040031;
-        public static final int login_logging=0x7f04002f;
-        public static final int login_notauser=0x7f04002e;
-        public static final int login_password_hint=0x7f04002c;
-        public static final int login_pleasewait=0x7f040030;
+        public static final int login_button_text=0x7f04002e;
+        public static final int login_internet_error=0x7f040033;
+        public static final int login_invalid=0x7f040032;
+        public static final int login_logging=0x7f040030;
+        public static final int login_notauser=0x7f04002f;
+        public static final int login_password_hint=0x7f04002d;
+        public static final int login_pleasewait=0x7f040031;
         /**  Login 
          */
-        public static final int login_username_hint=0x7f04002b;
+        public static final int login_username_hint=0x7f04002c;
         public static final int menu_settings=0x7f040009;
         public static final int menu_settings_book_list=0x7f040010;
         public static final int menu_settings_detalles_libro=0x7f040018;
-        public static final int searchbook_scanbutton=0x7f040029;
-        public static final int searchbook_searchbox_hint=0x7f04002a;
+        public static final int menu_share_detalles_libro=0x7f04001d;
+        public static final int searchbook_scanbutton=0x7f04002a;
+        public static final int searchbook_searchbox_hint=0x7f04002b;
         /**  Search Book 
          */
-        public static final int searchbook_searchby=0x7f040028;
-        public static final int set_confirm_password_hint=0x7f040043;
-        public static final int set_new_password_hint=0x7f040042;
-        public static final int set_new_username_hint=0x7f040041;
-        public static final int set_submitButton_text=0x7f040044;
-        public static final int signup_already_exists_error=0x7f04003e;
-        public static final int signup_button_text=0x7f040039;
-        public static final int signup_confirm_pass_error=0x7f04003c;
-        public static final int signup_confirm_password_hint=0x7f040036;
-        public static final int signup_fullname_hint=0x7f040037;
-        public static final int signup_internet_error=0x7f04003f;
-        public static final int signup_password_hint=0x7f040035;
-        public static final int signup_pleasewait=0x7f04003b;
-        public static final int signup_sending=0x7f04003a;
-        public static final int signup_user_created=0x7f04003d;
-        public static final int signup_username_hint=0x7f040034;
-        public static final int signup_zipcode_hint=0x7f040038;
+        public static final int searchbook_searchby=0x7f040029;
+        public static final int set_confirm_password_hint=0x7f040044;
+        public static final int set_new_password_hint=0x7f040043;
+        public static final int set_new_username_hint=0x7f040042;
+        public static final int set_submitButton_text=0x7f040045;
+        public static final int signup_already_exists_error=0x7f04003f;
+        public static final int signup_button_text=0x7f04003a;
+        public static final int signup_confirm_pass_error=0x7f04003d;
+        public static final int signup_confirm_password_hint=0x7f040037;
+        public static final int signup_fullname_hint=0x7f040038;
+        public static final int signup_internet_error=0x7f040040;
+        public static final int signup_password_hint=0x7f040036;
+        public static final int signup_pleasewait=0x7f04003c;
+        public static final int signup_sending=0x7f04003b;
+        public static final int signup_user_created=0x7f04003e;
+        public static final int signup_username_hint=0x7f040035;
+        public static final int signup_zipcode_hint=0x7f040039;
         public static final int subitem1=0x7f04000a;
         public static final int subitem1_book_list=0x7f040011;
         public static final int subitem1_detalles_libro=0x7f040019;
@@ -206,7 +208,7 @@ public final class R {
         public static final int tab3=0x7f040008;
         /**  About 
          */
-        public static final int title_activity_about=0x7f040045;
+        public static final int title_activity_about=0x7f040046;
         public static final int title_activity_book_activity=0x7f040017;
         /**  Book 
          */
@@ -214,37 +216,37 @@ public final class R {
         /**  BookList 
          */
         public static final int title_activity_book_list=0x7f04000f;
-        public static final int title_activity_buscar_libros=0x7f040020;
+        public static final int title_activity_buscar_libros=0x7f040021;
         /**  Crossing 
          */
-        public static final int title_activity_crossing=0x7f04001f;
+        public static final int title_activity_crossing=0x7f040020;
         /**  CrossingList 
          */
-        public static final int title_activity_crossing_list=0x7f04001e;
-        public static final int title_activity_prueba_internet=0x7f040022;
-        public static final int title_activity_resultado_busqueda_libros=0x7f040021;
+        public static final int title_activity_crossing_list=0x7f04001f;
+        public static final int title_activity_prueba_internet=0x7f040023;
+        public static final int title_activity_resultado_busqueda_libros=0x7f040022;
         /**  Settings 
          */
-        public static final int title_activity_settings=0x7f040040;
+        public static final int title_activity_settings=0x7f040041;
         /**  Signup 
          */
-        public static final int title_activity_sign_up=0x7f040033;
+        public static final int title_activity_sign_up=0x7f040034;
         /**  Tabs 
          */
         public static final int title_activity_tabs=0x7f040005;
-        public static final int title_activity_uj=0x7f040023;
+        public static final int title_activity_uj=0x7f040024;
         /**  User 
          */
-        public static final int title_activity_user=0x7f040027;
+        public static final int title_activity_user=0x7f040028;
         /**  User Profile 
          */
-        public static final int title_activity_user_profile=0x7f04004c;
-        public static final int title_testitem_detail=0x7f040024;
-        public static final int title_testitem_list=0x7f040025;
+        public static final int title_activity_user_profile=0x7f04004d;
+        public static final int title_testitem_detail=0x7f040025;
+        public static final int title_testitem_list=0x7f040026;
         /**  Class Libro 
          */
-        public static final int unknown_field=0x7f04004e;
-        public static final int userprofile_viewuserbooks=0x7f04004d;
+        public static final int unknown_field=0x7f04004f;
+        public static final int userprofile_viewuserbooks=0x7f04004e;
     }
     public static final class style {
         /** 
--- src/com/example/boox/BookActivity.java
+++ src/com/example/boox/BookActivity.java
@@ -3,6 +3,7 @@ package com.example.boox;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -46,6 +47,13 @@ public class BookActivity extends FragmentActivity {
                 //startActivity(parentActivityIntent);
                 finish();
                 return true;
+            case R.id.share:
+            	Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+            	sharingIntent.setType("text/html");
+            	sharingIntent.putExtra(
+            			android.content.Intent.EXTRA_TEXT, 
+            			Html.fromHtml("<p>elcresan just viewed Juego de Tronos on BooX</p>"));
+            	startActivity(Intent.createChooser(sharingIntent, "Share with"));
         }
         return super.onOptionsItemSelected(item);
     }
